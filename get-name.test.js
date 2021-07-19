2e942ff69a751e6aa11ee8569c3f431000f7cfeb
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-19 16:13:46 -0700

    added getName test

---
+++ get-name.test.js
@@ -0,0 +1,9 @@
+describe('getName', () => {
+  it('gets a name from an object with a name key', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+    const expected = 'spot';
+    const actual = getName(spot);
+
+    expect(actual).toEqual(expected);
+  });
+});
