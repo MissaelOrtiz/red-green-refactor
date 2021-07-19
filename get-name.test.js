7d6a273a625060bf235623940f967844666fa329
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-19 16:22:12 -0700

    added another test to validate getName function, ALL GREEN

--- get-name.test.js
+++ get-name.test.js
@@ -1,10 +1,18 @@
-import getName from "./get-name";
+import getName from './get-name';
 
 describe('getName', () => {
-  it('gets a name from an object with a name key', () => {
+  it('gets a name from an object with a name key [spot]', () => {
     const spot = { name: 'spot', age: 5, weight: '20 lbs' };
     const expected = 'spot';
     const actual = getName(spot);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('gets a name from an object with a name key [character]', () => {
+    const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    const expected = 'Aang';
+    const actual = getName(character);
 
     expect(actual).toEqual(expected);
   });
