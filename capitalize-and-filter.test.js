eedbea385bb5760dce34e45d1ca376889f737f18
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-19 17:33:30 -0700

    added in test validation for capitalizeAndFilter

--- capitalize-and-filter.test.js
+++ capitalize-and-filter.test.js
@@ -1,9 +1,17 @@
 import capitalizeAndFilter from './capitalize-and-filter';
 
 describe('capitalizeAndFilter', () => {
-  it('takes in an array and filters', () => {
+  it('takes in an array and filters strings that start with f, capitalizing the rest [validation]', () => {
     const strings = ['free', 'fifty percent off', 'something else', 'not on sale'];
     const expected = ['SOMETHING ELSE', 'NOT ON SALE'];
+    const actual = capitalizeAndFilter(strings);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('takes in an arrya and filters strings that start with f, capitalizing the rest [validation]', () => {
+    const strings = ['something', 'wicked', 'this', 'way', 'comes', 'froyo'];
+    const expected = ['SOMETHING', 'WICKED', 'THIS', 'WAY', 'COMES'];
     const actual = capitalizeAndFilter(strings);
 
     expect(actual).toEqual(expected);
